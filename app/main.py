9d4ee0d9a90aed26eaf840b977daef29ea397bfb
VioletHarmon2002 <liizasabanova@mail.ru>
2026-05-08 22:40:01 +0300

    enable CORS for frontend

--- app/main.py
+++ app/main.py
@@ -8,7 +8,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "https://people.arcada.fi",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
